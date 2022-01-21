57542357943197fe8122f99b703d084dc5acc39f
RFNoC <rfnoc@rfnocs-pro-51.lan>
2022-01-21 06:56:56 -0800

    Required Tables - in place

---
+++ data/migrations/20220121140311_first-migration.js
@@ -0,0 +1,54 @@
+
+exports.up = async function(knex) {
+    await knex.schema
+        .createTable('projects', table => { 
+            table.increments('project_id')
+            table.text('project_name', 128).notNullable()
+            table.string('project_description', 256)
+            table.boolean('project_completed').defaultTo(0)
+        })
+        .createTable('resources', table => { 
+            table.increments('resource_id')
+            table.text('resource_name', 128).notNullable().unique()
+            table.string('resource_description', 256)
+        })
+        .createTable('tasks', table => { 
+            table.increments('task_id')
+            table.string('task_description', 256).notNullable()
+            table.text('task_notes', 128)
+            table.boolean('task_completed').defaultTo(0)
+            table.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('projects')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE')            
+        })
+        .createTable('project_resources', table => { 
+            table.increments('project_resource_id')
+            table.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('projects')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE')  
+            table.integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('resource_id')
+                .inTable('resources')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE') 
+        })
+  
+};
+
+exports.down = async function(knex) {
+    await knex.schema
+        .dropTableIfExists('project_resources')  
+        .dropTableIfExists('tasks')  
+        .dropTableIfExists('resources')  
+        .dropTableIfExists('projects')  
+};
