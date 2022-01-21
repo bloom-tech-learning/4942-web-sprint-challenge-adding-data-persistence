6abe716ad6546b690cc76732dd631db25b1d9a39
RFNoC <rfnoc@rfnocs-pro-51.lan>
2022-01-21 05:52:54 -0800

    Files to Complete - index.js api/server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
 // build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors')
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors()) // cors() returns a (req, res, nex) => { // stuff and then next() }
+
+server.use('/', (req, res)=>{
+    res.send('Unit 4 Week 2 Sprint Challenge!')
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    console.log('disaster!')
+    res.status(err.status || 500).json({
+    message: `The Horror: ${err.message}`,
+    })
+})
+
+module.exports = server;
